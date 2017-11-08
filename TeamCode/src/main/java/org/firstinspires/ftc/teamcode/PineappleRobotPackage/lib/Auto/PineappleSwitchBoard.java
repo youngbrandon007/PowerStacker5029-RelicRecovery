7e6e2ab96903748358a9b7a08db249e4689c5438
Krishna Suresh <krishna4132@gmail.com>
2017-11-08 17:53:52 -0500

    Switch board java doc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PineappleRobotPackage/lib/Auto/PineappleSwitchBoard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PineappleRobotPackage/lib/Auto/PineappleSwitchBoard.java
@@ -18,13 +18,22 @@ public class PineappleSwitchBoard {
     }
 
 
+    /**
+     * Simple method that reads the value of a digital switch and outputs it
+     * @param name The configuration name of the sensor
+     * @return The state of the digital switch
+     */
     public boolean loadDigital(String name){
         DigitalChannel dig = resources.hardwareMap.digitalChannel.get(name);
         return dig.getState();
     }
-
+    /**
+     * Simple method that reads the value of an analog sensor and outputs it
+     * @param name The configuration name of the sensor
+     * @return The state of the value
+     */
     public double loadAnalog(String name){
-        AnalogInput ana = resources.hardwareMap.analogInput.get(name);
-        return ana.getVoltage();
+        AnalogInput analogInput = resources.hardwareMap.analogInput.get(name);
+        return analogInput.getVoltage();
     }
 }
