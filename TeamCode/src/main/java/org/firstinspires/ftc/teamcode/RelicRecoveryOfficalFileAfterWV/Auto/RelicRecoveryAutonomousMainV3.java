585d493ff8194e9ff4e265cc1f5f9e5084b98e8e
Brandon-Laptop <robotgenis@gmail.com>
2017-12-12 10:48:40 -0500

    working on auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryOfficalFileAfterWV/Auto/RelicRecoveryAutonomousMainV3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryOfficalFileAfterWV/Auto/RelicRecoveryAutonomousMainV3.java
@@ -4,6 +4,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RelicRecoveryOfficalFile.RelicResources.RelicRecoveryEnums;
 import org.firstinspires.ftc.teamcode.RelicRecoveryOfficalFileAfterWV.RelicRecoveryResources.RelicRecoveryConfigV3;
 
+import java.util.ArrayList;
+
 /**
  * Created by Brandon on 12/5/2017.
  */
@@ -12,6 +14,8 @@ public class RelicRecoveryAutonomousMainV3 extends RelicRecoveryConfigV3 {
 
 
     autoCount count;
+
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -24,7 +28,6 @@ public class RelicRecoveryAutonomousMainV3 extends RelicRecoveryConfigV3 {
         //load everything
         aLoadSwitchBoard();
 
-
         //wait for start
         waitForStart();
         count.setCount("b");
@@ -34,28 +37,54 @@ public class RelicRecoveryAutonomousMainV3 extends RelicRecoveryConfigV3 {
                 case REDFRONT:
                     switch (count.getCount()){
                         case "b":
-                            //scan
+                            //TODO scan
+
                             break;
                         case "c":
-                            //jewels
+                            //TODO jewels
+
                             break;
                         case "d":
                             //drive off platform
+                            //TODO add encoder drive at the end
+                            if(driveOffPlatform(.5))
+                                count.setCount("e");
                             break;
                         case "e":
-                            //turn
+                            //TODO turn
+
                             break;
                         case "f":
-                            //line up to wall
+                            //TODO line up to wall
+
                             break;
                         case "g":
-                            //insert glyph
+                            //TODO insert glyph
+
                             break;
                         case "h":
-                            //collect glyph
+                            //only runs once
+                            //TODO get ready to collect glyph and track encoders
+
+
+                            count.setCount("i");
                             break;
                         case "i":
-                            //drive back using encoders
+                            //TODO drive to glyph and collect two
+                            encoderTracking.add();
+                            break;
+                        case "j":
+                            //only runs once
+                            //TODO stop encoders and
+
+                            count.setCount("k");
+                            break;
+                        case "k":
+                            //TODO drive back using encoders
+
+                            break;
+                        default:
+                            telemetry.addData("ERROR NOT LINE LETTER",count.getCount());
                             break;
                     }
                     break;
@@ -75,8 +104,28 @@ public class RelicRecoveryAutonomousMainV3 extends RelicRecoveryConfigV3 {
         }
     }
 
+    private int driveOff = 0;
+    public boolean driveOffPlatform(double motorSpeed){
+        robotHandler.drive.mecanum.update(motorSpeed, -motorSpeed);
+        double robotTilt = getTilt();
+        if(robotTilt > 3){
+            driveOff = 1;
+        }else if(robotTilt < 1 && driveOff == 1){
+            driveOff = 2;
+        }else if(driveOff == 3){
+            //TODO encoder drive then return true
+        }
+        telemetry.addData("Drive Off", driveOff);
+        return false;
+    }
 
+    public double getTilt(){
+        return 0.0;
+    }
 
+    public double getHeading(){
+        return 0.0;
+    }
 }
 
 class autoCount{
