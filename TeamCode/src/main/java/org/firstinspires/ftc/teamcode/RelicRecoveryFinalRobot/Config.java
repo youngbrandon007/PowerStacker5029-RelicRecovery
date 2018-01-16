69f58731868c8dc2468f710170308cbdbbf02d85
Brandon Young <robotgenis@gmail.com>
2018-01-16 16:28:49 -0500

    autonomous init updated with vuforia keyColumn scanning and gyro add

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryFinalRobot/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecoveryFinalRobot/Config.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.RelicRecoveryFinalRobot;
 
+import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.PineappleRobotPackage.lib.PineappleConfigLinearOpMode;
@@ -28,36 +29,51 @@ public abstract class Config extends PineappleConfigLinearOpMode{
     //SERVOS
     public PineappleServo servoFlipR;
     public PineappleServo servoFlipL;
-    //SEROVPOSITIONS
+
+    //SENSORS
+    //GYRO
+    public boolean calibration_complete = false;
+    public final int NAVX_DIM_I2C_PORT = 0;
+    public AHRS navx_device;
+    public final byte NAVX_DEVICE_UPDATE_RATE_HZ = 50;
 
     //SWITCH BOARD
     public RelicRecoveryEnums.AutoColor switchColor = RelicRecoveryEnums.AutoColor.RED;
     public RelicRecoveryEnums.StartingPosition switchPosition = RelicRecoveryEnums.StartingPosition.FRONT;
     public RelicRecoveryEnums.ColorPosition switchColorPosition = RelicRecoveryEnums.ColorPosition.REDFRONT;
-    public boolean switchDelayEnabled = true;
+    public boolean switchDelayEnabled = false;
     public double slideDelay = 0.0;
     //Still need detecting
-    public boolean switchJewels = true;
+    public boolean switchJewels = false;
     public boolean switchGlyphs = true;
-    public boolean switchGlyphWhite = true;
+    public boolean switchGlyphWhite = true; // ignored right now
     public boolean switchMoreGlyphs = false;
-    public boolean switchPID = true;
+    public boolean switchPID = false;
 
 
     @Override
     public void config(LinearOpMode linearOpMode) {
         robotHandler = new PineappleRobot(linearOpMode);
 
+        //DRIVE MOTORS
         driveFrontRight = robotHandler.motorHandler.newDriveMotor("FR", 1, false, false, PineappleEnum.MotorLoc.RIGHTFRONT, PineappleEnum.MotorType.NEV40);
         driveFrontLeft = robotHandler.motorHandler.newDriveMotor("FL", 1, false, false, PineappleEnum.MotorLoc.LEFTFRONT, PineappleEnum.MotorType.NEV40);
         driveBackRight = robotHandler.motorHandler.newDriveMotor("BR", 1, false, false, PineappleEnum.MotorLoc.RIGHTBACK, PineappleEnum.MotorType.NEV40);
         driveBackLeft = robotHandler.motorHandler.newDriveMotor("BL", 1, false, false, PineappleEnum.MotorLoc.LEFTBACK, PineappleEnum.MotorType.NEV40);
 
+        //MOTORS
         motorLift = robotHandler.motorHandler.newMotor("ML");
         motorCollect = robotHandler.motorHandler.newMotor("MC");
 
+        //SERVOS
         servoFlipL = robotHandler.servoHandler.newLimitServo( "SL", 202.5, Constants.flip.leftDown);
         servoFlipR = robotHandler.servoHandler.newLimitServo("SR",202.5, Constants.flip.rightDown);
+
+        //SENSORS
+        navx_device = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get("dim"),
+                NAVX_DIM_I2C_PORT,
+                AHRS.DeviceDataType.kProcessedData,
+                NAVX_DEVICE_UPDATE_RATE_HZ);
     }
 
     public void loadSwitchBoard(){
